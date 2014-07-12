68eb13914dc98a38c245e45147b2e6184ef83b02
Silent Penguin <silentpenguin@hotmail.co.uk>
2014-07-13 00:27:06 +0100

    Update HtmlHelperExtentions.cs

--- HtmlHelperExtentions.cs
+++ HtmlHelperExtentions.cs
@@ -5,9 +5,13 @@
 
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Web;
+using System.Web.Hosting;
 using System.Web.Mvc;
 using System.Web.Optimization;
 
@@ -27,33 +31,59 @@ namespace System.Web.Mvc
             }
 
             if (!scripts.Any(row => row.Path == path)){
-                scripts.Push(new ScriptFile(path));
+                if (path.Contains('*') || path.Contains('{'))
+                {
+                    IEnumerable<String> paths = Files(path);
+                    foreach (String file in paths)
+                    {
+                        scripts.Push(new ScriptFile(file));
+                    }
+                }
+                else
+                {
+                    scripts.Push(new ScriptFile(path));
+                }
             }
+        }
+
+        static String VersionToken = Regex.Escape("{version}");
+        static String VersionRegEx = @"(\d+(\s*\.\s*\d+){1,3})(-[a-z][0-9a-z-]*)?";
+        static String WildCardToken = Regex.Escape("*");
+        static String WildCardRegEx = @".*";
+
+        private static IEnumerable<String> Files(String path)
+        {
+            String directoryPath = Path.GetDirectoryName(path);
+            String fileName = Path.GetFileName(path);
+            VirtualDirectory directory = BundleTable.VirtualPathProvider.GetDirectory(directoryPath);
+
+            String pattern = Regex.Escape(path)
+                .Replace(WildCardToken, WildCardRegEx)
+                .Replace(VersionToken, VersionRegEx);
+            Regex regex = new Regex(pattern);
+
+            return directory.Files.Cast<VirtualFile>()
+                .Where(file => regex.IsMatch("~" + file.VirtualPath))
+                .Select(file => "~" + file.VirtualPath);
         }
 
         public static IHtmlString RenderScripts(this HtmlHelper htmlHelper, Boolean useBundles = true)
         {
             Stack<ScriptFile> scripts = htmlHelper.ViewContext.HttpContext.Items[ScriptsKey] as Stack<ScriptFile>;
+            IEnumerable<String> notBundles = scripts.Where(script => !script.IsBundle).Select(file => file.Path);
             if (scripts != null)
             {
                 StringBuilder builder = new StringBuilder();
-                UrlHelper urlHelper = new UrlHelper(htmlHelper.ViewContext.RequestContext, htmlHelper.RouteCollection);
                 Int32 count = scripts.Count();
 
                 if (useBundles)
                 {
                     BundleCollection bundles = BundleTable.Bundles;
-                    BundleContext bundleContext = new BundleContext(htmlHelper.ViewContext.HttpContext, bundles, "/");
                     foreach (Bundle bundle in bundles)
                     {
-                        IEnumerable<String> bundleFiles = bundle.EnumerateFiles(bundleContext).Select(file => file.VirtualFile.VirtualPath);
+                        IEnumerable<String> bundleFiles = BundleResolver.Current.GetBundleContents(bundle.Path);
 
-                        if (scripts.Any(script => script.Path == bundle.Path))
-                        {
-                            builder.AppendLine(Scripts.Render(bundle.Path).ToString());
-                            scripts.Single(file => file.Path == bundle.Path).Rendered = true;
-                        }
-                        else if (bundleFiles.All(file => scripts.Any(script => script.Path == file)))
+                        if (bundleFiles.Intersect(notBundles).Count() == bundleFiles.Count())
                         {
                             builder.AppendLine(Scripts.Render(bundle.Path).ToString());
                             foreach(ScriptFile script in scripts.Where(script => bundleFiles.Any(file => file == script.Path)))
@@ -64,14 +94,11 @@ namespace System.Web.Mvc
                     }
                 }
 
-                for (Int32 i = 0; i < count; i++)
+                if (scripts.Any(script => !script.Rendered))
                 {
-                    ScriptFile context = scripts.Pop();
-                    if (!context.Rendered)
-                    {
-                        builder.AppendLine("<script type='text/javascript' src='" + urlHelper.Content(context.Path) + "'></script>");
-                    }
+                    builder.AppendLine(Scripts.Render(scripts.Where(script => !script.Rendered).Select(script => script.Path).ToArray()).ToString());
                 }
+
                 return new MvcHtmlString(builder.ToString());
             }
 
@@ -82,9 +109,11 @@ namespace System.Web.Mvc
     public class ScriptFile {
         public String Path { get; set; }
         public Boolean Rendered { get; set; }
+        public Boolean IsBundle { get; set; }
         public ScriptFile (String path)
         {
             Path = path;
+            IsBundle = BundleResolver.Current.IsBundleVirtualPath(path);
             Rendered = false;
         }
     }
